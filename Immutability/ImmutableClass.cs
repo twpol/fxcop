32da59895d20a758cf75e8fd44c8ef9005da689a
James Ross <silver@warwickcompsoc.co.uk>
2009-12-02 03:21:02 +0000

    Implement 3 different tests of immutability:  - Non-readonly, non-static field.  - Non-static property setter.  - Mutable type used in any field or property.

--- Immutability/ImmutableClass.cs
+++ Immutability/ImmutableClass.cs
@@ -20,5 +20,43 @@ namespace Immutability {
 				return TargetVisibilities.All;
 			}
 		}
+
+		bool TypeIsImmutable(TypeNode type) {
+			foreach (var attribute in type.Attributes) {
+				if (attribute.Type.FullName == "Jgr.ImmutableAttribute") {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public override ProblemCollection Check(Member member) {
+			if (TypeIsImmutable(member.DeclaringType)) {
+				var field = member as Field;
+				var property = member as PropertyNode;
+				if (field != null) {
+					if (!field.IsStatic) {
+						if (!field.IsInitOnly) {
+							this.Problems.Add(new Problem(GetNamedResolution("ReadonlyField", member.FullName)));
+						}
+						if (!TypeIsImmutable(field.Type)) {
+							this.Problems.Add(new Problem(GetNamedResolution("MutableMemberType", member.FullName, field.Type.FullName)));
+						}
+					}
+				} else if (property != null) {
+					if (!property.IsStatic) {
+						if (property.Setter != null) {
+							if ((property.Setter.Flags & MethodFlags.Private) == 0) {
+								this.Problems.Add(new Problem(GetNamedResolution("PropertySetter", member.FullName)));
+							}
+						}
+						if (!TypeIsImmutable(property.Type)) {
+							this.Problems.Add(new Problem(GetNamedResolution("MutableMemberType", member.FullName, property.Type.FullName)));
+						}
+					}
+				}
+			}
+			return this.Problems;
+		}
 	}
 }
