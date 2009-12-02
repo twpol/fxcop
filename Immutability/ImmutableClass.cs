03d75a4a989ef8ca046b2f6ce45ae53bfeb05fea
James Ross <silver@warwickcompsoc.co.uk>
2009-12-02 02:37:08 +0000

    Initial import of FxCop rules.

---
+++ Immutability/ImmutableClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.FxCop.Sdk;
+
+namespace Immutability {
+	public class ImmutableClass : BaseIntrospectionRule {
+		public ImmutableClass()
+			: base("ImmutableClass", "Immutability.FxCop", typeof(ImmutableClass).Assembly) {
+		}
+
+		public override TargetVisibilities TargetVisibility {
+			get {
+				return TargetVisibilities.All;
+			}
+		}
+	}
+}
