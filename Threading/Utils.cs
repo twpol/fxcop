1f7f5d5407fa9fa251892d6e09f0157b21203e87
James Ross <git@james-ross.co.uk>
2015-12-13 22:24:09 +0000

    Update website address in headers

--- Threading/Utils.cs
+++ Threading/Utils.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//------------------------------------------------------------------------------
+// http://james-ross.co.uk/projects/fxcop
+// License: New BSD License (BSD).
+//------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
